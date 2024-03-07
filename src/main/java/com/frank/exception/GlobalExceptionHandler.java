d68eaafc5ec1993c13398a7be607eb674d6b83ec
weedsx <87897205+weedsx@users.noreply.github.com>
2024-03-07 21:24:54 +0800

    Update Directory name

---
+++ src/main/java/com/frank/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.frank.exception;
+
+import com.frank.common.BaseResponse;
+import com.frank.common.ErrorCode;
+import com.frank.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author
+ * @from
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
---
+++ src/main/java/com/frank/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.frank.mapper;
+
+import com.frank.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author
+ * @from
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
